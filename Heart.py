8c56792292825e227b839c37f3096c97054787e2
petermatts <mpeters0514@gmail.com>
2022-11-11 14:27:49 -0500

    Added Code

---
+++ Heart.py
@@ -0,0 +1,62 @@
+from turtle import *
+import math
+
+RAD2DEGREE = math.pi/180
+
+# makes heart with two full semi-circles at top
+def draw1(angle = 50, length = 200):
+    color('red')
+    begin_fill()
+
+    # calc variables
+    circportion = 270 - angle
+    radius = length*math.tan(((90-angle)/2)*RAD2DEGREE)
+
+    # draw
+    setheading(angle)
+    forward(length)
+    circle(radius, circportion)
+    setheading(90)
+    circle(radius, circportion)
+    setheading(360-angle)
+    forward(length)
+
+    hideturtle()
+    end_fill()
+    done()
+
+# makes a heart with two semicircles at the top where the angle at the point where
+# the semi-circles meet is inpointangle
+def draw2(angle = 50, inpointangle = 90, length = 200):
+    color('red')
+    title('Love')
+    begin_fill()
+
+    # calc variables
+    halfpointang = inpointangle/2
+    circportion = 270 - halfpointang - angle
+    radius = length*math.cos((angle*RAD2DEGREE))/(math.cos(halfpointang*RAD2DEGREE)+math.sin(angle*RAD2DEGREE))
+
+    height = radius + radius*math.cos(angle*RAD2DEGREE) + length*math.sin(angle*RAD2DEGREE)
+    
+    penup()
+    setposition(position()[0], -height/2)
+    pendown()
+
+    # draw
+    setheading(angle)
+    forward(length)
+    circle(radius, circportion)
+    setheading(-heading())
+    circle(radius, circportion)
+    setheading(360-angle)
+    forward(length)
+
+    hideturtle()
+    end_fill()
+    done()
+
+
+if __name__ == '__main__':
+    # draw1(50, 200)
+    draw2(50, 70, 400)
