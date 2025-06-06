877acbeac24035344488cfbfd149dee0bef896b4
Matthew Peters <mpeters0514@gmail.com>
2025-06-06 03:01:57 -0400

    Slight build fixes for windows

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-@echo off
+@REM @echo off
 pyinstaller --add-data=.\images\Heart.png:. --icon=.\images\Heart.png --onefile --noconsole Heart.py
-copy .\dist\Heart.exe %USERPROFILE%\Desktop
+COPY /B /Y .\dist\Heart.exe %USERPROFILE%\Desktop\
+COPY /B /Y .\dist\Heart.exe %USERPROFILE%\OneDrive\Desktop\
