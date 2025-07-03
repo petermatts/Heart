3bde887ab00e861845695fd8516fb3be7df4b8fa
Matthew Peters <mpeters0514@gmail.com>
2025-07-02 20:56:16 -0400

    Fixing requirements

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @REM @echo off
-pyinstaller --add-data=.\images\Heart.png:. --icon=.\images\Heart.png --onefile --noconsole Heart.py
+CALL .\.venv\Scripts\activate
+.\.venv\Scripts\pyinstaller.exe --add-data=.\images\Heart.png:. --icon=.\images\Heart.png --onefile --noconsole Heart.py
+deactivate
 COPY /B /Y .\dist\Heart.exe %USERPROFILE%\Desktop\
 COPY /B /Y .\dist\Heart.exe %USERPROFILE%\OneDrive\Desktop\
--- build.command
+++ build.command
@@ -1,2 +1,4 @@
-pyinstaller -y --add-data=./images/Heart.png:. --icon=./images/Heart.png --onedir --noconsole Heart.py
+source ./.venv/bin/activate
+./.venv/bin/pyinstaller -y --add-data=./images/Heart.png:. --icon=./images/Heart.png --onedir --noconsole Heart.py
+deactivate
 cp -rf ./dist/Heart.app /Users/$USER/Desktop/
